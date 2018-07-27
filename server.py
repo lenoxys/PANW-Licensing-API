cd9634e5c1387998c85fd0c13a78aebea13b7da3
Aurelien Delamarre <daedalus.helios@gmail.com>
2018-07-27 21:33:17 +0200

    second step

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 # Copyright (c) 2018, Palo Alto Networks
 #
@@ -18,15 +18,24 @@ import sys
 import os
 import argparse
 import logging
+import json
+
+import urllib.request
+import urllib.parse
+
+try:
+    from config import *
+except ImportError:
+    for arg in sys.argv: 1
+# use the command line to call the function from a single script.
+    print ("Run setup.py")
+    sys.exit(0)
 
 from pandevice import firewall
 from pandevice import panorama
 from var_dump import var_dump
 
-# Panorama Conf
-pn_hostname = '10.0.0.2'
-pn_api_username = 'admin'
-pn_api_password = 'admin'
+url = 'https://api.paloaltonetworks.com/api/license/activate'
 
 # Firewall Conf
 fw_hostname = '10.0.3.100'
@@ -38,10 +47,9 @@ pn = None
 def main():
 
     # Get command line arguments
-    parser = argparse.ArgumentParser(description="Upgrade a Palo Alto Networks Firewall or Panorama to the specified version")
+    parser = argparse.ArgumentParser(description="Third party license server API validation for VM without Internet access")
     parser.add_argument('-v', '--verbose', action='count', help="Verbose (-vv for extra verbose)")
     parser.add_argument('-q', '--quiet', action='store_true', help="No output")
-    parser.add_argument('-n', '--dryrun', action='store_true', help="Print what would happen, but don't perform upgrades")
 
     args = parser.parse_args()
 
@@ -60,7 +68,7 @@ def main():
             logging_format = '%(message)s'
         logging.basicConfig(format=logging_format, level=logging_level)
 
-    print "Connecting!"
+    print ("Connecting!")
     global pn, pn_hostname, pn_api_username, pn_api_password
     #pn = panorama.Panorama(pn_hostname, pn_api_username, pn_api_password)
     #pprint (pn.op("show devices all"))
@@ -71,10 +79,57 @@ def main():
     resp = fw.op("show system info")
 
     for t in resp.iter('vm-uuid'):
-        print t.text
+        uuid = t.text
 
     for t in resp.iter('vm-cpuid'):
-        print t.text
+        cpuid = t.text
+    
+    print ("cpuid: %s" % cpuid)    
+    print ("uuid: %s" % uuid)
+
+    register_vm(cpuid, uuid)
+
+def register_vm(cpuid, uuid):
+    global authcode, url, api
+
+    data =  urllib.parse.urlencode({ "cpuid" : cpuid , "uuid" : uuid ,"authCode" : authcode })
+    data = data.encode('ascii')
+
+    req = urllib.request.Request(url=url, data=data)
+    req.add_header( 'apikey', api )
+
+    var_dump (req)
+
+    r = urllib.request.urlopen(req)
+
+    dname = "plop"
+
+    var_dump (r)
+
+    for x in r:
+        resp = json.loads(x)
+        c = (len(resp))
+
+    i=0
+    ## while statement added to address the issues with the fact the auto focus licences
+    ## does not have a partidfield to work with
+    ## so instead look at the feature Field and if autoforcus
+    ## manual set the file name.
+    while i < c:
+        if resp[i]['featureField'] == ('AutoFocus Device License'):
+            fName = dname+"-PAN-VM-autofocus.key"
+            file = open(fName,"w") 
+            file.write(resp[i]['keyField'])
+            file.close() 
+            i+=1
+        else:
+            fName = dname+"-"+resp[i]['partidField']+".key"
+            file = open(fName,"w") 
+            file.write(resp[i]['keyField'])
+            file.close() 
+            i+=1
+        
+    r.close()
 
 if __name__== "__main__":
     main()
---
+++ setup.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+
+# Copyright (c) 2018, Palo Alto Networks
+#
+# Permission to use, copy, modify, and/or distribute this software for any
+# purpose with or without fee is hereby granted, provided that the above
+# copyright notice and this permission notice appear in all copies.
+#
+# THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+# WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+# MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+# ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+# WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+# ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+# OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+
+import sys
+
+def main():
+
+    f = open('config.py', 'w')
+
+    print ("Licensing API Key")
+    
+    print ("The license API key provides users access to the various license functions through REST APIs . It authenticates the users that access the licensing APIs and one key can be used for all license API calls.")
+    print ("Super User permissions are required to access the Enable link below to regenerate or disable the license key.")
+    print ("You'll find this key under Palo Alto Networks Support Website : Assets > Licensing API Key")
+
+    api = raw_input("Licensing API Key: ")
+    f.write("api = '%s'\n" % api) 
+
+    api = raw_input("Panorama IP or FQDN: ")
+    f.write("pn_hostname = '%s'\n" % api) 
+
+    api = raw_input("Panorama API username: ")
+    f.write("pn_api_username = '%s'\n" % api) 
+
+    api = raw_input("Panorama API password: ")
+    f.write("pn_api_password = '%s'\n" % api) 
+
+    f.close()
+
+if __name__== "__main__":
+    main()
